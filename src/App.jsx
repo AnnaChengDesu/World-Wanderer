ff915a296bdaddcfc5e3cf9c0c4c056584dc1b58
zhenganna <zhengan@appexnetworks.com>
2023-11-23 16:39:15 +0800

    feat: scroll to top if enter new page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import BaseLayout from "pages/components/BaseLayout/BaseLayout";
-import { Suspense, lazy } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Suspense, lazy, useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 const Home = lazy(() => import("./pages/Home/Home"));
 const CountryDetail = lazy(() => import("./pages/CountryDetail/CountryDetail"));
 const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage"));
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
   return (
     <BaseLayout>
       <Suspense fallback={<div>Loading...</div>}>
