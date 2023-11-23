b6d2c2c646c45014550fa95175e21e39708d7e1f
zhenganna <zhengan@appexnetworks.com>
2023-11-23 15:45:05 +0800

    feat: filter countries by name and region

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import {
   FormControl,
@@ -18,19 +18,57 @@ import useInfiniteLoader from "hooks/useInfiniteLoader";
 
 function Home() {
   const { data = [], isLoading } = useGetAllCountriesQuery();
-  const [{ name, region }, setFilter] = useState({
+  const [listData, setListData] = useState([]);
+  const [filter, setFilter] = useState({
     name: "",
     region: "",
   });
 
-  const { items, hasMoreItems, loadMoreRef } = useInfiniteLoader(data);
+  const { items, hasMoreItems, loadMoreRef } = useInfiniteLoader(listData);
 
-  const handleChangeFilter = (key) => (event) => {
+  useEffect(() => {
+    if (data.length === 0) return;
+    const filteredListData = data
+      .filter(
+        (country) =>
+          filter.region === "" || country.region.includes(filter.region)
+      )
+      .filter(
+        (country) =>
+          filter.name === "" ||
+          country.name.toLowerCase().includes(filter.name.toLowerCase())
+      );
+
+    setListData(filteredListData);
+  }, [data, filter.name, filter.region]);
+
+  const onChangeFilter = (key) => (event) => {
     const value = event.target.value;
     setFilter((state) => ({
       ...state,
       [key]: value,
     }));
+  };
+
+  const renderCountryCards = () => {
+    if (isLoading) return <Skeleton.HomeCountryCards />;
+
+    if (items.length === 0) {
+      return (
+        <div>
+          Sorry, we couldn't find a match country for "{filter.name}" in &#8203;
+          {filter.region || "the world"}.
+        </div>
+      );
+    }
+
+    return (
+      <div className={styles.cards}>
+        {items.map((country) => (
+          <CountryCard key={country.alpha3Code} {...country}></CountryCard>
+        ))}
+      </div>
+    );
   };
 
   return (
@@ -39,23 +77,23 @@ function Home() {
         <FormControl fullWidth className={styles.search}>
           <OutlinedInput
             type="search"
-            value={name}
+            value={filter.name}
             startAdornment={
               <InputAdornment position="start">
                 <SearchIcon />
               </InputAdornment>
             }
             placeholder="Search for a country..."
-            onChange={handleChangeFilter("name")}
+            onChange={onChangeFilter("name")}
           />
         </FormControl>
-        <FormControl fullWidth className={styles.filterRegion}>
+        <FormControl fullWidth className={styles.filter}>
           <InputLabel id="filter-region-label">Filter by region</InputLabel>
           <Select
             labelId="filter-region-label"
             label="Filter by region"
-            value={region}
-            onChange={handleChangeFilter("region")}
+            value={filter.region}
+            onChange={onChangeFilter("region")}
           >
             <MenuItem value="">All</MenuItem>
             {REGION_LIST.map((region) => (
@@ -66,15 +104,8 @@ function Home() {
           </Select>
         </FormControl>
       </div>
-      {isLoading ? (
-        <Skeleton.HomeCountryCards />
-      ) : (
-        <div className={styles.cards}>
-          {items.map((country) => (
-            <CountryCard key={country.alpha3Code} {...country}></CountryCard>
-          ))}
-        </div>
-      )}
+
+      {renderCountryCards()}
       {hasMoreItems && (
         <div ref={loadMoreRef} className={styles.loadMore}></div>
       )}
